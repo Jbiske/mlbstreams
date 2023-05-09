142d097acc2fb17bc84559fea78bf90f8cf708e4
Josh <biscuitemail2@gmail.com>
2023-05-09 18:43:06 -0400

    yes

--- src/pages/pittsburg.js
+++ src/pages/pittsburg.js
@@ -5,7 +5,7 @@ import React from "react"
 const Pittsburg  = () => {
     return( 
         
-            <iframe src='https://embedstream.me/pittsburgh-pirates-stream-1' width='500px' height='500px' scrolling='no' frameborder='0' allowfullscreen='true'  allowtransparency referrerpolicy='unsafe-url'></iframe>
+            <iframe src='https://embedstream.me/pittsburgh-pirates-stream-1' title='yes' width='500px' height='500px' scrolling='no' frameborder='0' allowfullscreen='true'  allowtransparency referrerpolicy='unsafe-url'></iframe>
        
 
 
