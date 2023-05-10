9ee9fd52df383ff27ea29d6ecf80ef0280856b46
Josh <biscuitemail2@gmail.com>
2023-05-09 20:48:50 -0400

    m fixed

--- src/pages/twins.js
+++ src/pages/twins.js
@@ -5,15 +5,15 @@ import React from "react";
 const Twins = () => {
     return(
         <div>
-      <h1>Minnasota Twins</h1>
+      <h1>Minnesota Twins</h1>
       <h2> Broadcast 1 </h2>
       <div className="iframe">
-      <iframe src='https://embedstream.me/minnasota-twins-stream-1'title='Baseball' scrolling='no'allowFullScreen ></iframe>
+      <iframe src='https://embedstream.me/minnesota-twins-stream-1'title='Baseball' scrolling='no'allowFullScreen ></iframe>
      
       </div>
       <h2> Broadcast 2 </h2>
       <div className="iframe">
-      <iframe src='https://embedstream.me/minnasota-twins-stream-2'title='Baseball' scrolling='no'allowFullScreen ></iframe>
+      <iframe src='https://embedstream.me/minnesota-twins-stream-2'title='Baseball' scrolling='no'allowFullScreen ></iframe>
      
       </div>
         </div>
