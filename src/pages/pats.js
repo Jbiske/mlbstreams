8dee1e5b07e6662ede9de06e0e250896433e002f
Josh <biscuitemail2@gmail.com>
2024-04-16 19:33:40 -0400

    wash

--- src/pages/pats.js
+++ src/pages/pats.js
@@ -9,7 +9,7 @@ const Pats = () => {
         
             <div className="iframe"> 
           
-            <iframe src='https://embedstreams.me/nfl/washington-capitols-stream-1'title='Baseball'scrolling='no' allowFullScreen  ></iframe>
+            <iframe src='https://embedstreams.me/nhl/washington-capitals-stream-1'title='Baseball'scrolling='no' allowFullScreen  ></iframe>
             </div>
          
         </div>
